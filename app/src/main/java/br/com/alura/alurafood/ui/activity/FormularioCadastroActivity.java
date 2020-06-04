194b038ded9f1cfc4bb608f6709feb22e4fbd13b
gabrieldarlan <gabriel.darlan88@gmail.com>
2020-06-04 13:29:23 -0300

    validando dados do formulario

--- app/src/main/java/br/com/alura/alurafood/ui/activity/FormularioCadastroActivity.java
+++ app/src/main/java/br/com/alura/alurafood/ui/activity/FormularioCadastroActivity.java
@@ -1,8 +1,12 @@
 package br.com.alura.alurafood.ui.activity;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.material.textfield.TextInputLayout;
 
 import br.com.alura.alurafood.R;
 
@@ -13,5 +17,64 @@ public class FormularioCadastroActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_cadastro);
         setTitle("Formulário chavoso");
+
+        inicializaCampos();
+
     }
+
+    private void inicializaCampos() {
+        configuraCampoNomeCompleto();
+        configuraCampoCpf();
+        configuraCampoTelefoneComDdd();
+        configuraCampoEmail();
+        configuraCampoSenha();
+    }
+
+    private void configuraCampoSenha() {
+        TextInputLayout textInputSenha = findViewById(R.id.formulario_cadastro_campo_senha);
+        adicionaValidacaoPadrao(textInputSenha);
+    }
+
+    private void configuraCampoEmail() {
+        TextInputLayout textInputEmail = findViewById(R.id.formulario_cadastro_campo_email);
+        adicionaValidacaoPadrao(textInputEmail);
+    }
+
+    private void configuraCampoTelefoneComDdd() {
+        TextInputLayout textInputTelefoneComDdd = findViewById(R.id.formulario_cadastro_campo_telefone_com_ddd);
+        adicionaValidacaoPadrao(textInputTelefoneComDdd);
+    }
+
+    private void configuraCampoCpf() {
+        TextInputLayout textInputCpf = findViewById(R.id.formulario_cadastro_campo_cpf);
+        adicionaValidacaoPadrao(textInputCpf);
+    }
+
+    private void configuraCampoNomeCompleto() {
+        TextInputLayout textInputNomeCompleto = findViewById(R.id.formulario_cadastro_campo_nome_completo);
+        adicionaValidacaoPadrao(textInputNomeCompleto);
+    }
+
+    private void adicionaValidacaoPadrao(final TextInputLayout textInputCampo) {
+        final EditText campo = textInputCampo.getEditText();
+        campo.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                String texto = campo.getText().toString();
+                if (!hasFocus) {
+                    validaCampoObrigatorio(texto, textInputCampo);
+                }
+            }
+        });
+    }
+
+    private void validaCampoObrigatorio(String texto, TextInputLayout textInputCampo) {
+        if (texto.isEmpty()) {
+            textInputCampo.setError("Campo obrigatório");
+        } else {
+            textInputCampo.setError(null);
+            textInputCampo.setErrorEnabled(false);
+        }
+    }
+
 }
